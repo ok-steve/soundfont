6cf8e6c65024ec67fab09513285f7c83c9ce84ba
Steve Cherry <stevenccherry@gmail.com>
2016-01-26 08:14:08 -0500

    Change configuration paths

--- js/config-localhost.js
+++ js/config-localhost.js
@@ -1,3 +1,29 @@
-requirejs({
-  deps: ['main']
-});
+var require = {
+  // baseUrl: '/js',
+  paths: {
+    angular: '../bower_components/angular/angular.min',
+    ngRoute: '../bower_components/angular-route/angular-route.min',
+
+    mocha: '../bower_components/mocha/mocha',
+    chai: '../bower_components/chai/chai',
+    sinon: '../bower_components/sinon/lib/sinon'
+  },
+  shim: {
+    angular: {
+      exports: 'angular'
+    },
+    ngRoute: {
+      deps: ['angular']
+    },
+
+    mocha: {
+      init: function () {
+        'use strict';
+
+        this.mocha.setup('bdd');
+
+        return this.mocha;
+      }
+    }
+  }
+};
--- js/config.js
+++ js/config.js
@@ -1,3 +1,28 @@
-requirejs({
-  deps: ['main']
-});
+var require = {
+  paths: {
+    angular: 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.8/angular.min',
+    ngRoute: 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.8/angular-route.min',
+
+    mocha: 'https://cdnjs.cloudflare.com/ajax/libs/mocha/2.3.4/mocha.min',
+    chai: 'https://cdnjs.cloudflare.com/ajax/libs/chai/3.4.1/chai.min',
+    sinon: 'https://cdnjs.cloudflare.com/ajax/libs/sinon.js/1.15.4/sinon.min'
+  },
+  shim: {
+    angular: {
+      exports: 'angular'
+    },
+    ngRoute: {
+      deps: ['angular']
+    },
+
+    mocha: {
+      init: function () {
+        'use strict';
+
+        this.mocha.setup('bdd');
+
+        return this.mocha;
+      }
+    }
+  }
+};
