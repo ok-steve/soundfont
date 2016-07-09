c3a11ff075435d3f37077af356275accb457b63f
Steve Cherry <stevenccherry@gmail.com>
2016-07-09 10:14:22 -0400

    Code formatting

--- js/main.js
+++ js/main.js
@@ -7,8 +7,14 @@ require([
   if (navigator.hasOwnProperty('serviceWorker')) {
     navigator.serviceWorker.register('/service-worker.js', {
       scope: '/'
+    }).then(response => {
+      console.log(reponse);
+    }).catch(error => {
+      console.log(error);
     });
   }
 
-  angular.bootstrap(document, ['app']);
+  angular.bootstrap(document, [
+    'app'
+  ]);
 });
