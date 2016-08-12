28a822107ca98e003088788367de0acdbe408bee
Steve Cherry <stevenccherry@gmail.com>
2016-08-12 19:21:52 -0400

    Simplify keyboard input

--- src/js/resources/keyboard-input.js
+++ src/js/resources/keyboard-input.js
@@ -1,34 +1,34 @@
 import keyboardJS from 'keyboardjs';
-import { each, keys } from './utilities/underscore';
+import { keys } from './utilities/underscore';
 import { mtof } from './utilities/midi';
 
+const keyMap = {
+  'a': 60, // a:C
+  'w': 61, // w:C#/Db
+  's': 62, // s:D
+  'e': 63, // e:D#/Eb
+  'd': 64, // d:E
+  'f': 65, // f:F
+  't': 66, // t:F#/Gb
+  'g': 67, // g:G
+  'y': 68, // y:G#/Ab
+  'h': 69, // h:A
+  'u': 70, // u:A#/Bb
+  'j': 71, // j:B
+  'k': 72  // k:C
+};
+
 export function bind( keydown, keyup ) {
-  const keyMap = {
-    'a': 60, // a:C
-    'w': 61, // w:C#/Db
-    's': 62, // s:D
-    'e': 63, // e:D#/Eb
-    'd': 64, // d:E
-    'f': 65, // f:F
-    't': 66, // t:F#/Gb
-    'g': 67, // g:G
-    'y': 68, // y:G#/Ab
-    'h': 69, // h:A
-    'u': 70, // u:A#/Bb
-    'j': 71, // j:B
-    'k': 72  // k:C
-  };
+  keyboardJS.bind( keys( keyMap ), e => {
+    e.preventRepeat();
 
-  each( keys( keyMap ), key => {
-    let freq = mtof( keyMap[key] );
+    const freq = mtof( keyMap[e.key] );
 
-    keyboardJS.bind( key, e => {
-      e.preventRepeat();
+    keydown( freq );
+  }, e => {
+    const freq = mtof( keyMap[e.key] );
 
-      keydown( freq );
-    }, e => {
-      keyup( freq );
-    });
+    keyup( freq );
   });
 }
 
