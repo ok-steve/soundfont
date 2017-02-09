b08d568a9b0a6f8a05178f406cbfb9b3932455d7
Steve Cherry <stevenccherry@gmail.com>
2017-02-08 19:37:22 -0500

    Remove global state

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,11 @@
 import { autoinject } from 'aurelia-framework';
 import { Router, RouterConfiguration } from 'aurelia-router';
 
-import { KeyboardService } from './resources/services/keyboard';
-import { SynthService } from './resources/services/synth';
-
 import { routes } from './shell/routes';
 
 @autoinject
 export class App {
   public router: Router;
-  keyboard: KeyboardService;
-  synth: SynthService;
-
-  constructor( keyboard: KeyboardService, synth: SynthService ) {
-    this.keyboard = keyboard;
-    this.synth = synth;
-  }
 
   configureRouter( config: RouterConfiguration, router: Router ) {
     config.title = 'Synthia';
@@ -23,13 +13,5 @@ export class App {
     config.map( routes );
 
     this.router = router;
-  }
-
-  activate() {
-    this.keyboard.bind();
-  }
-
-  deactivate() {
-    this.keyboard.unbind();
   }
 }
