80d350697e334e4954b3b75ded66525e059bcee1
Steve Cherry <stevenccherry@gmail.com>
2017-05-16 17:57:08 -0400

    Remove progressive webapp configuration

--- src/main.js
+++ src/main.js
@@ -14,13 +14,3 @@ export function configure( aurelia ) {
 
   aurelia.start().then(() => aurelia.setRoot());
 }
-
-if ( navigator.hasOwnProperty('serviceWorker') ) {
-  navigator.serviceWorker.register( '/service-worker.js', {
-    scope: '/'
-  }).then(response => {
-    console.log( response );
-  }).catch(error => {
-    console.log( error );
-  });
-}
