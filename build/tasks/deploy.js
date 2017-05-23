915bbd8950fea7e17cd072a65fb35c01517c5d75
Steve Cherry <stevenccherry@gmail.com>
2017-05-23 18:32:16 -0400

    Prepare for deployment

---
+++ build/tasks/deploy.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var paths = require('../paths');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('deploy', ['export'], function() {
+  return gulp.src(paths.exportSrv + '/**/*')
+    .pipe(ghPages());
+});
