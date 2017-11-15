68ae336b88b356ff1d09c4e4094fda44f1d43990
Steve Cherry <stevenccherry@gmail.com>
2017-11-15 07:05:53 -0500

    Make envelope values floats

--- src/scripts/actions/setEnvelope.js
+++ src/scripts/actions/setEnvelope.js
@@ -3,7 +3,7 @@ import { SET_ENVELOPE } from '../constants/index';
 const setEnvelope = (key, value) => ({
   type: SET_ENVELOPE,
   value: {
-    [key]: value,
+    [key]: parseFloat(value),
   },
 });
 
