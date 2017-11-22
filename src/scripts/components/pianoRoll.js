4d7c80f59e3d99af9dbfa1a284136fe94b253e40
Steve Cherry <stevenccherry@gmail.com>
2017-11-22 06:35:47 -0500

    Make piano roll more accessible

--- src/scripts/components/pianoRoll.js
+++ src/scripts/components/pianoRoll.js
@@ -1,40 +1,43 @@
-import { toMessage } from '../lib/Util';
+import { toMessage, pitchToMIDI } from '../lib/Util';
 import fromEvent from '../lib/fromEvent';
 import merge from '../lib/merge';
 import store from '../store';
 
 const el = document.querySelector('.piano-roll');
 
+const isSpaceOrEnter = keyCode => keyCode === 32 || keyCode === 13;
+
 const toNote = (e) => {
   const { octave } = store.getState();
 
-  return (12 * octave) + parseInt(e.target.dataset.note, 10);
+  return pitchToMIDI(octave, parseInt(e.target.dataset.note, 10));
 };
 
-const pointerdown = target => merge(
-  fromEvent(target, 'mousedown'),
-  fromEvent(target, 'touchstart'),
+const noteon = merge(
+  fromEvent(el, 'mousedown'),
+  fromEvent(el, 'touchstart'),
+  fromEvent(el, 'keydown').filter(e => !e.repeat).filter(e => isSpaceOrEnter(e.keyCode)),
 );
 
-const pointerup = target => merge(
-  fromEvent(target, 'mouseup'),
-  fromEvent(target, 'touchend'),
+const noteoff = merge(
+  fromEvent(el, 'mouseup'),
+  fromEvent(el, 'touchend'),
+  fromEvent(el, 'keyup').filter(e => isSpaceOrEnter(e.keyCode)),
 );
-
-const noteon = pointerdown(el).map((e) => {
-  e.target.classList.add('is-pressed');
-
-  return e;
-}).map(toNote).map(note => toMessage(144, note));
-
-const noteoff = pointerup(el).map((e) => {
-  e.target.classList.remove('is-pressed');
-  return e;
-}).map(toNote).map(note => toMessage(128, note));
 
 const pianoRoll = merge(
-  noteon,
-  noteoff,
+  noteon.map(toNote).map(note => toMessage(144, note)),
+  noteoff.map(toNote).map(note => toMessage(128, note)),
 );
 
 export default pianoRoll;
+
+noteon.subscribe((e) => {
+  e.preventDefault();
+  e.target.classList.add('is-pressed');
+});
+
+noteoff.subscribe((e) => {
+  e.preventDefault();
+  e.target.classList.remove('is-pressed');
+});
