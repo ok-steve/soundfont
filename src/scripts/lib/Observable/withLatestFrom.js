01a874ff95e4e3614cd5bf0a7cdb3a78875692b4
Steve Cherry <stevenccherry@gmail.com>
2019-05-07 10:27:53 -0400

    Correctly sort array

--- src/scripts/lib/Observable/withLatestFrom.js
+++ src/scripts/lib/Observable/withLatestFrom.js
@@ -1,31 +1,40 @@
 import Observable from 'zen-observable';
 
-const toArray = iter => Array.from(iter)
-  .sort((x, y) => x[0] > y[0])
-  .map(x => x[1]);
+const toArray = iter =>
+Array.from(iter)
+    .sort((x, y) => x[0] - y[0])
+    .map(x => x[1]);
 
-const withLatestFrom = (...sources) => new Observable((observer) => {
-  if (sources.length === 0) { return Observable.from([]); }
+const withLatestFrom = (...sources) =>
+  new Observable((observer) => {
+    if (sources.length === 0) {
+      return Observable.from([]);
+    }
 
-  let count = sources.length;
-  const values = new Map();
+    let count = sources.length;
+    const values = new Map();
 
-  const subscriptions = sources.map((source, index) => Observable.from(source).subscribe({
-    next(v) {
-      values.set(index, v);
-      if (values.size === sources.length && index === 0) { observer.next(toArray(values)); }
-    },
-    error(e) {
-      observer.error(e);
-    },
-    complete() {
-      count -= 1;
+    const subscriptions = sources.map((source, index) =>
+      Observable.from(source).subscribe({
+        next(v) {
+          values.set(index, v);
+          if (values.size === sources.length && index === 0) {
+            observer.next(toArray(values));
+          }
+        },
+        error(e) {
+          observer.error(e);
+        },
+        complete() {
+          count -= 1;
 
-      if (count === 0) { observer.complete(); }
-    },
-  }));
+          if (count === 0) {
+            observer.complete();
+          }
+        },
+      }));
 
-  return () => subscriptions.forEach(s => s.unsubscribe());
-});
+    return () => subscriptions.forEach(s => s.unsubscribe());
+  });
 
 export default withLatestFrom;
