0024201c3c17eb49e2e612dd2b35b4787135da97
Steve Cherry <stevenccherry@gmail.com>
2019-05-29 16:30:19 -0400

    Change icon

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-Soundfont
-=========
+# Soundfont
 
 A soundfont player.
 
-References
-----------
+## References
 
 1. https://www.smashingmagazine.com/2015/12/reimagining-single-page-applications-progressive-enhancement
+2. Piano icon: https://thenounproject.com/search/?q=piano&i=2526073
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,14 @@
-module.exports = (eleventyConfig) => {
+module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy('src/assets');
   eleventyConfig.addPassthroughCopy('src/browserconfig.xml');
-  eleventyConfig.addPassthroughCopy('src/favicon.ico');
-  eleventyConfig.addPassthroughCopy('src/manifest.json');
+  eleventyConfig.addPassthroughCopy('src/site.webmanifest');
   eleventyConfig.addPassthroughCopy('src/service-worker.js');
 
   return {
     dir: {
       input: 'src',
       output: 'dist',
-      includes: 'templates',
-    },
+      includes: 'templates'
+    }
   };
 };
