0fb5c5e742ffc0b808e015362eec3ea0beb1f12f
Steve Cherry <stevenccherry@gmail.com>
2019-09-03 11:07:01 -0400

    Minify html in production

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,24 @@
+const htmlmin = require('html-minifier');
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.setWatchJavaScriptDependencies(false);
   eleventyConfig.addPassthroughCopy('src/assets');
   eleventyConfig.addPassthroughCopy('src/service-worker.js');
+
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+    if (process.env.ELEVENTY_ENV === 'production' && outputPath.endsWith('.html')) {
+      const minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyJs: true,
+      });
+
+      return minified;
+    }
+
+    return content;
+  });
 
   return {
     pathPrefix:
